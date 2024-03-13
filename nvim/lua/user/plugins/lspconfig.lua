cae72c2aae10c5b086c890f19c11d3a210ed40be
Sebastian Bach Andersen <ostehandler@hotmail.dk>
2024-03-13 14:47:30 +0100

    use react compatible lsp

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -22,26 +22,6 @@ return {
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-    -- PHP
-    -- require('lspconfig').intelephense.setup({
-    --   commands = {
-    --     IntelephenseIndex = {
-    --       function()
-    --         vim.lsp.buf.execute_command({ command = 'intelephense.index.workspace' })
-    --       end,
-    --     },
-    --   },
-    --   on_attach = function(client, bufnr)
-    --     client.server_capabilities.documentFormattingProvider = false
-    --     client.server_capabilities.documentRangeFormattingProvider = false
-    --     -- if client.server_capabilities.inlayHintProvider then
-    --     --   vim.lsp.buf.inlay_hint(bufnr, true)
-    --     -- end
-    --   end,
-    --   capabilities = capabilities
-    -- })
-    --
-
     require('lspconfig').rust_analyzer.setup({
       on_attach = on_attach,
       capabilities = capabilities,
@@ -80,20 +60,9 @@ return {
       --   ['textDocument/publishDiagnostics'] = function() end
       -- }
     })
-
-    -- Vue, JavaScript, TypeScript
-    require('lspconfig').volar.setup({
-      on_attach = function(client, bufnr)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-        -- if client.server_capabilities.inlayHintProvider then
-        --   vim.lsp.buf.inlay_hint(bufnr, true)
-        -- end
-      end,
-      capabilities = capabilities,
-      -- Enable "Take Over Mode" where volar will provide all JS/TS LSP services
-      -- This drastically improves the responsiveness of diagnostic updates on change
-      filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    require('lspconfig').tsserver.setup({
+      on_attach = on_attach,
+      filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx', 'javascript' }
     })
 
     require('lspconfig').omnisharp.setup({
